a954dc5cd0e895c73ac1bc8d87c92d53f0b089d8
Robert Kerr <robertcrawleykerr@gmail.com>
2016-06-03 15:28:41 -0500

    user interction and for loop function practice #1

---
+++ public/scripts/user-interaction.js
@@ -0,0 +1,33 @@
+// TODO: Ask the user for their name.
+//       Keep asking if an empty input is provided.
+
+"use strict";
+do {
+	var name = prompt("What is you name?")
+} while (name == "");
+
+
+// TODO: Show an alert message that welcomes the user based on their input.
+
+if (typeof name == 'string') {
+	alert("Hello " + name + "!");
+
+}
+
+// TODO: Ask the user if they like pizza.
+//       Based on their answer show a creative alert message.
+
+
+	
+var pizza = confirm("Do you likea da pizza?");
+
+if (pizza == true) {
+	alert("This is correct.");
+
+} else {
+	alert("Your opinion is invalid, bro....");
+
+}
+
+
+
