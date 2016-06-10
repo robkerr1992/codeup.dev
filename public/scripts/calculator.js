c4222e38df0b829bda87b5aa105e3d8000c2af77
Robert Kerr <robertcrawleykerr@gmail.com>
2016-06-10 16:50:42 -0500

    Making a calculator exercises: calculator completed!!! :D

---
+++ public/scripts/calculator.js
@@ -0,0 +1,75 @@
+var firstInput = document.getElementById('firstInput');
+var secondInput = document.getElementById('secondInput');
+var operandInput = document.getElementById('operand');
+var numbers = document.getElementsByClassName('numberBtn');
+var operands = document.getElementsByClassName('operandBtn');
+var clear = document.getElementById('clearBtn');
+var equals = document.getElementById('equalsBtn');
+var decimal = document.getElementById('decimalBtn');
+var backspace = document.getElementById('backspaceBtn');
+var numberSpitter = function () {
+	var numberSelected = this.getAttribute('data-value');
+	if (operandInput.value !== "") {
+		numberSelected = this.getAttribute('data-value');
+		secondInput.value += numberSelected;
+	} else {
+		firstInput.value += numberSelected;
+	}
+}
+var operandSpitter = function () {
+	var operandSelected = this.getAttribute('data-value');
+	operandInput.value = operandSelected;
+}
+var processNumbers = function() {
+	if (operandInput.value == '+') {
+		firstInput.value = (parseInt(firstInput.value)) + (parseInt(secondInput.value));
+		operandInput.value = "";
+		secondInput.value = "";
+	} else if (operandInput.value == '-') {
+		firstInput.value = (parseInt(firstInput.value)) - (parseInt(secondInput.value));
+		operandInput.value = "";
+		secondInput.value = "";
+	} else if (operandInput.value == '*') {
+		firstInput.value = (parseInt(firstInput.value)) * (parseInt(secondInput.value));
+		operandInput.value = "";
+		secondInput.value = "";
+
+	} else if (operandInput.value == '/') {
+		firstInput.value = (parseInt(firstInput.value)) / (parseInt(secondInput.value));
+		operandInput.value = "";
+		secondInput.value = "";
+	}
+}
+var clearAllFields = function () {
+	firstInput.value = ""
+	operandInput.value = ""
+	secondInput.value = ""
+}
+var decimalPlacer = function () {
+	if (operandInput.value == "" && firstInput.value.indexOf('.') == -1) {
+		firstInput.value += '.';
+	} if (operandInput.value !== "" && secondInput.value.indexOf('.') == -1) {
+		secondInput.value += '.';
+	}
+}
+var backspacer = function () {
+	if (operandInput.value == "" && firstInput.value !== "") {
+		firstInput.value = firstInput.value.substring(0, firstInput.value.length - 1);
+
+	} else if (secondInput.value !== "") {
+		secondInput.value = secondInput.value.substring(0, secondInput.value.length - 1);
+
+	} else if (firstInput.value !== "" && operandInput.value !== "") {
+		operandInput.value = "";
+	}
+}
+for (var i = 0; i < this.numbers.length; i++) {
+numbers[i].addEventListener('click', numberSpitter, false);
+}
+for (var i = 0; i < this.operands.length; i++) {
+operands[i].addEventListener('click', operandSpitter, false);
+}
+equals.addEventListener('click', processNumbers, false);
+clear.addEventListener('click', clearAllFields, false);
+decimal.addEventListener('click', decimalPlacer, false);
+backspace.addEventListener('click', backspacer, false);
