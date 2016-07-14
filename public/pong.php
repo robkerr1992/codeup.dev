13ce5a860b0536089ba92af0bac87e42c468125a
Robert Kerr <robertcrawleykerr@gmail.com>
2016-07-14 16:30:03 -0500

    completed ping/pong exercise

---
+++ public/pong.php
@@ -0,0 +1,33 @@
+<?php
+function pageController()
+{
+    var_dump($_GET);
+    $data = array();
+    if (isset($_GET['count'])) {
+        $data['count'] = $_GET['count'];
+    } else {
+        $data['count'] = 0;
+    };
+
+    return $data;
+}
+
+;
+extract(pageController());
+?>
+
+
+<!DOCTYPE html>
+<head>
+    <title>Ping</title>
+</head>
+<body>
+<h1>Number of Hits:<?= $count ?></h1>
+<p>
+    <a href="ping.php?count=<?= $count + 1 ?>">Hit</a>
+</p>
+<p>
+    <a href="pong.php?count=0">Miss</a>
+</p>
+
+</body>
