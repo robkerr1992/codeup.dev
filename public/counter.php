db08594b74e2319b6aab5c3325d3c50ac7af45f8
Robert Kerr <robertcrawleykerr@gmail.com>
2016-07-14 15:50:57 -0500

    completed counter exercise

--- public/counter.php
+++ public/counter.php
@@ -1,19 +1,33 @@
 <?php
 function pageController() {
+var_dump($_GET);
     $data = array();
+    if(isset($_GET['number'])) {
+        $data['number'] = $_GET['number'];
+    } else {
+        $data['number'] = 0;
+    };
 
     return $data;
-}
+};
 
 extract(pageController());
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Come Get Some</title>
+</head>
+<body>
+    <h1>Counter: <?= $number ?></h1>
+    <p>
+        <a href="?number=<?= $number + 1 ?>"  name="Up">Up</a>
+    </p>
+    <p>
+        <a href="?number=<?= $number - 1 ?>" name="Down">Down</a>
+    </p>
 
+</body>
+</html>
 
-
-/**
- * Created by PhpStorm.
- * User: MacBookPro
- * Date: 7/14/16
- * Time: 2:46 PM
- */
