498ffbc49c47f1cc8e72b4e2ca0423074dc9bee9
Robert Kerr <robertcrawleykerr@gmail.com>
2016-07-14 14:55:54 -0500

    inital commit

---
+++ public/counter.php
@@ -0,0 +1,19 @@
+<?php
+function pageController() {
+    $data = array();
+
+    return $data;
+}
+
+extract(pageController());
+?>
+
+
+
+
+/**
+ * Created by PhpStorm.
+ * User: MacBookPro
+ * Date: 7/14/16
+ * Time: 2:46 PM
+ */
